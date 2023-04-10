077bb6d54da1eba538e473fedfc3e798752dc6e5
Jose Rinconez <rinconez_08@hotmail.com>
2023-04-10 15:17:45 -0700

    video cards done!

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,7 +7,7 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
     
   return (
-    <Card sx={{ width: { md: '320px', xs: '100%' }}}>
+    <Card sx={{ width: { md: '310px', xs: '100%' }, boxShadow: 'none', borderRadius: 0}}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
             <CardMedia 
                 image={snippet?.thumbnails?.high?.url}
@@ -25,11 +25,6 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
             <Typography variant="subtitle2" fontWeight="bold" color="#afcfcf">
                 {snippet?.channelTitle || demoChannelTitle}
                 <CheckCircle sx={{ fontSize: 12, color: "#afcfcf", ml: '5px'}}/>
-            </Typography>
-        </Link>
-        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-            <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
-                {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
             </Typography>
         </Link>
         </CardContent>
